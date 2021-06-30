b0577da92fdc5ed934b20e0dacc7fcff068fd8cc
Jorian <jorian@mail.ru>
2021-06-30 14:07:52 +0300

    rabota 30.06 - 3

--- js/scripts.js
+++ js/scripts.js
@@ -12,5 +12,22 @@ $(function(){
                 height: ($(this).parent().find('.submenu a').length * 24)
             }, 1000);
         }
-   });
+    });
+    
+    $('button.basket').click(function(){
+        $('.popup-desk').addClass('active');
+    });
+    
+    $('.popup-desk').click(function(e){
+        if (e.target == this) {
+            $(this).removeClass('active');
+        }
+    });
+    
+    $('.register').click(function(e){
+        e.preventDefault();
+        
+        $('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p>')
+    });
 });
+
